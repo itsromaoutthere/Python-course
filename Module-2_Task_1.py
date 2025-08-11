d0ae95f2948bc8e31656518b9950160817884069
Roma <romaa29032006@gmail.com>
2025-08-11 06:45:10 +0000

    Module 2

---
+++ Module-2_Task_1.py
@@ -0,0 +1,17 @@
+#Task 1: Perform Basic Mathematical Operations
+'''
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+	Addition
+	Subtraction
+	Multiplication
+	Division
+3.  Displays the results of each operation on the screen.
+'''
+first_number=int(input("Enter the first number:"))
+second_number=int(input("Enter the first number:"))
+print("Addition:",first_number+second_number)
+print("Subtraction:",first_number-second_number)
+print("Multiplication:",first_number*second_number)
+print("Division:",first_number/second_number)
